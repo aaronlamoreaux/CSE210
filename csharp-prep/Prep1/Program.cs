af86e034b4f5def0d9754136541dd03092967efd
aaronlamoreaux <lam22013@byui.edu>
2023-04-18 09:16:47 -0700

    chang

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string n1 = Console.ReadLine(); 
+
+        Console.Write("what is your last name? ");
+        string n2 = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {n2}, {n1} {n2}. ");
     }
 }
