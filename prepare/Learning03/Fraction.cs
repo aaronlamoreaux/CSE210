84171e7616ca30e36ab0b0fa3ca5343f8edc325e
aaronlamoreaux <lam22013@byui.edu>
2023-05-19 16:15:42 -0700

    week05

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,42 @@
+public class Fraction {
+    private int _top = 1;
+    private int _bottom = 1;
+
+    public Fraction(){
+    }
+
+    public Fraction(int whole) {
+        _top = whole;
+    }
+
+    public Fraction(int top, int bottom){
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop() {
+        return _top;
+    }
+
+    public void SetTop(int top) {
+        _top = top;
+    }
+
+    public int GetBottom() {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom) {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString() {
+        string fraction = _top + "/" + _bottom;
+        return fraction;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
