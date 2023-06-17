782771c2c7534f00063461180856650057b0f721
aaronlamoreaux <lam22013@byui.edu>
2023-06-16 19:56:00 -0700

    Week 09

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 4);
+        
+        Rectangle rectangle = new Rectangle("green", 4, 3);
+
+        Circle circle = new Circle("red", 10);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes){
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"the {color} shape has an area of {area}.");
+        }
     }
 }
